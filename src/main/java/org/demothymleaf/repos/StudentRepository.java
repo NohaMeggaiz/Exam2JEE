40386351776d1fc86395bf40d747c9db2a6a144d
noha <nohameggaiz@gmail.com>
2024-06-07 11:49:33 +0100

    new commit

--- src/main/java/org/demothymleaf/repos/StudentRepository.java
+++ src/main/java/org/demothymleaf/repos/StudentRepository.java
@@ -1,9 +1,6 @@
 package org.demothymleaf.repos;
 
-import org.demothymleaf.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
